3034776d7f4b7ad79e27edbd944504958a8266af
romintomasetti <romin.tomasetti@gmail.com>
2025-09-19 15:36:36 +0000

    cicd(stretegy): expose compiler version and cuda architecture id
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- .github/workflows/strategy.py
+++ .github/workflows/strategy.py
@@ -1,6 +1,7 @@
 import argparse
 import json
 import logging
+import pprint
 
 def full_image(*, name : str, tag : str, args : argparse.Namespace) -> str:
     """
@@ -14,9 +15,14 @@ def complete_job(partial : dict, args : argparse.Namespace) -> dict:
     """
     logging.info(f'Completing job {partial}.')
 
-    partial['cmake_preset'] = '-'.join(partial['compiler_family'])
+    name = 'cuda-' + '-'.join(partial['compiler_family'][0][0])
 
-    name = 'cuda-' + partial['cmake_preset']
+    if len(partial['compiler_family']) > 1 and partial['compiler_family'][1][0] == 'nvcc':
+        name += '-nvcc'
+        partial['compiler_family'][1] = ('nvcc', partial['cuda_version'])
+
+    partial['cmake_preset'] = '-'.join([x[0] for x in partial['compiler_family']])
+
     tag = f'{partial['cuda_version']}-devel-ubuntu24.04'
 
     partial['base_image'] = f'nvidia/cuda:{tag}'
@@ -33,20 +39,22 @@ def main(*, args : argparse.Namespace) -> None:
 
     matrix.append(complete_job({
         'cuda_version' : '12.8.1',
-        'compiler_family' : ['gcc', 'nvcc'],
-        'cuda_arch' : 'volta70',
+        'compiler_family' : [('gcc', '13'), ('nvcc',)],
+        'cuda_arch' : ('volta', '70'),
     }, args = args))
 
     matrix.append(complete_job({
         'cuda_version' : '13.0.0',
-        'compiler_family' : ['gcc', 'nvcc'],
-        'cuda_arch' : 'blackwell120',
+        'compiler_family' : [('gcc', '13'), ('nvcc',)],
+        'cuda_arch' : ('blackwell', '120'),
     }, args = args))
 
     # Labels for testing runners.
     for job in matrix:
-        runs_on = ['self-hosted', 'linux', 'docker', 'amd64', job['cuda_arch'], 'gpu:0']
+        runs_on = ['self-hosted', 'linux', 'docker', 'amd64', ''.join(job['cuda_arch']), 'gpu:0']
         job['runs-on'] = runs_on
+
+    logging.info(f'Strategy matrix:\n{pprint.pformat(matrix)}')
 
     print(f'matrix={json.dumps(matrix, default = str)}')
     print(f'environment={json.dumps(environment, default = str)}')

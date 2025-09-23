e1a1fce37e77c3735e31c2165c1e964366aaf6d7
romintomasetti <romin.tomasetti@gmail.com>
2025-09-23 08:21:44 +0000

    tests(binaries): resource usage
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ tests/python/tools/test_binaries/saxpy.cu
@@ -0,0 +1,11 @@
+#include "cuda.h"
+
+//! Kernel that performs a @c saxpy.
+__global__ void saxpy(const float cst_a, const float* __restrict__ const d_x, float* __restrict__ const d_y, const unsigned int size)
+{
+    const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (idx < size) {
+        d_y[idx] = cst_a * d_x[idx] + d_y[idx];
+    }
+}

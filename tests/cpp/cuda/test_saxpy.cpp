f19ead462b34d49cb914634b33d06bc7bbd590bf
romintomasetti <romin.tomasetti@gmail.com>
2025-09-25 09:34:06 +0000

    tools(nsys): nsys session helper for running, exporting and parsing the results
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/cpp/cuda/test_saxpy.cpp
+++ tests/cpp/cuda/test_saxpy.cpp
@@ -13,7 +13,7 @@
 using index_t = decltype(dim3::x);
 
 __global__
-void saxpy(const index_t size, const float factor, const float* __restrict__ const vec_x, float* __restrict__ const vec_y)
+void saxpy_kernel(const index_t size, const float factor, const float* __restrict__ const vec_x, float* __restrict__ const vec_y)
 {
     const index_t index = blockIdx.x * blockDim.x + threadIdx.x;
     if(index < size)
@@ -24,36 +24,38 @@ int main()
 {
     constexpr index_t size = 1024;
 
-    cudaStream_t stream = nullptr;
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamCreate(&stream));
+    cudaStream_t stream_A = nullptr, stream_B = nullptr;
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamCreate(&stream_A));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamCreate(&stream_B));
 
     float* vec_x = nullptr;
     float* vec_y = nullptr;
 
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMallocAsync(&vec_x, size * sizeof(float), stream)); 
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMallocAsync(&vec_y, size * sizeof(float), stream));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMallocAsync(&vec_x, size * sizeof(float), stream_A)); 
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMallocAsync(&vec_y, size * sizeof(float), stream_B));
 
     {
         const std::vector<float> vec_x_h(size, 1.f), vec_y_h(size, 2.f);
-        CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(vec_x, vec_x_h.data(), size * sizeof(float), cudaMemcpyHostToDevice));
-        CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(vec_y, vec_y_h.data(), size * sizeof(float), cudaMemcpyHostToDevice));
-        CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamSynchronize(stream));
+        CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(vec_x, vec_x_h.data(), size * sizeof(float), cudaMemcpyHostToDevice, stream_A));
+        CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(vec_y, vec_y_h.data(), size * sizeof(float), cudaMemcpyHostToDevice, stream_B));
+        CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamSynchronize(stream_A));
     }
 
     constexpr index_t block_size = 128;
     constexpr index_t grid_size  = (size + block_size - 1) / block_size;
 
-    saxpy<<<dim3{grid_size, 1, 1}, dim3{block_size, 1, 1} , 0, stream>>>(size, 2.f, vec_x, vec_y);
+    saxpy_kernel<<<dim3{grid_size, 1, 1}, dim3{block_size, 1, 1} , 0, stream_B>>>(size, 2.f, vec_x, vec_y);
 
     std::vector<float> result(size);
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(result.data(), vec_y, size * sizeof(float), cudaMemcpyDeviceToHost, stream));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaMemcpyAsync(result.data(), vec_y, size * sizeof(float), cudaMemcpyDeviceToHost, stream_B));
 
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamSynchronize(stream));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamSynchronize(stream_B));
 
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaFreeAsync(vec_x, stream));
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaFreeAsync(vec_y, stream));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaFreeAsync(vec_x, stream_A));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaFreeAsync(vec_y, stream_B));
 
-    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamDestroy(stream));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamDestroy(stream_A));
+    CUDA_HELPERS_CHECK_CUDART_CALL(cudaStreamDestroy(stream_B));
 
     for(const auto& elm : result) {
         if(elm != 4.f) throw std::runtime_error("wrong value");
---
+++ tests/python/tools/test_nsys.py
@@ -0,0 +1,136 @@
+import logging
+import os
+import pathlib
+import typing
+
+import pandas
+import semantic_version
+import typeguard
+
+from cuda_helpers.tools.nsys import Report, Session, strip_cuda_api_suffix
+
+class TestSession:
+    """
+    Test :py:class:`cuda_helpers.tools.nsys.Session`.
+    """
+    TMPDIR = pathlib.Path(os.environ['CMAKE_CURRENT_BINARY_DIR'])
+    EXECUTABLE = pathlib.Path(os.environ['CMAKE_BINARY_DIR']) / 'tests' / 'cpp' / 'cuda' / 'tests_cuda_saxpy'
+
+    @staticmethod
+    @typeguard.typechecked
+    def single_row_to_dict(*, data : pandas.DataFrame) -> dict[str, typing.Any]:
+        """
+        Check that `data` has one row, and convert it to a `dict`.
+        """
+        assert len(data) == 1, data
+        return data.squeeze().to_dict()
+
+    def run(self) -> Session:
+        assert self.EXECUTABLE.is_file()
+
+        ns = Session(
+            output_dir = self.TMPDIR,
+            output_file_prefix = self.EXECUTABLE.name,
+        )
+
+        ns.run(
+            cmd = [self.EXECUTABLE],
+            cwd = self.TMPDIR,
+        )
+
+        ns.export_to_sqlite()
+
+        return ns
+
+    def test_cuda_api_trace(self):
+        """
+        Collect all `Cuda` API calls of :cpp:file:`tests/cpp/cuda/test_saxpy.cpp`.
+        """
+        ns = self.run()
+
+        cuda_api_trace = ns.extract_statistical_report(report = 'cuda_api_trace')
+
+        logging.info(cuda_api_trace['Name'])
+
+        expt = [
+            'cuModuleGetLoadingMode',
+            'cudaStreamCreate',
+            'cudaStreamCreate',
+            'cudaMallocAsync',
+            'cudaMallocAsync',
+            'cudaMemcpyAsync',
+            'cudaMemcpyAsync',
+            'cudaStreamSynchronize',
+        ]
+
+        if semantic_version.Version(os.environ['CUDA_VERSION']) in semantic_version.SimpleSpec('>=13.0.0'):
+            expt += [
+                'cuLibraryLoadData',
+                'cuLibraryGetKernel',
+                'cuKernelGetName',
+            ]
+
+        expt += [
+            'cudaLaunchKernel',
+            'cudaMemcpyAsync',
+            'cudaStreamSynchronize',
+            'cudaFreeAsync',
+            'cudaFreeAsync',
+            'cudaStreamDestroy',
+            'cudaStreamDestroy',
+        ]
+
+        assert list(map(strip_cuda_api_suffix, cuda_api_trace['Name'].to_list())) == expt
+
+    def test_report(self):
+        """
+        Process the `nsys` report with :py:class:`cuda_helpers.tools.nsys.Report`.
+        """
+        ns = self.run()
+
+        cuda_api_trace = ns.extract_statistical_report(report = 'cuda_api_trace')
+
+        with Report(db = ns.output_file_sqlite) as report:
+
+            logging.info(f'Tables are {report.tables}.')
+
+            cupti_activity_kind_synchronization = report.table(name = 'CUPTI_ACTIVITY_KIND_SYNCHRONIZATION')
+
+            logging.info(cupti_activity_kind_synchronization)
+
+            cuda_stream_synchronize = cuda_api_trace[cuda_api_trace['Name'].str.startswith('cudaStreamSynchronize')]
+
+            logging.info(cuda_stream_synchronize)
+
+            assert len(cuda_stream_synchronize) == 2
+
+            # Each call to 'cudaStreamSynchronize' targets a distinct stream.
+            stream_id_A = self.single_row_to_dict(data = cupti_activity_kind_synchronization[cupti_activity_kind_synchronization['correlationId'] == cuda_stream_synchronize.iloc[0]['CorrID']])['streamId']
+            stream_id_B = self.single_row_to_dict(data = cupti_activity_kind_synchronization[cupti_activity_kind_synchronization['correlationId'] == cuda_stream_synchronize.iloc[1]['CorrID']])['streamId']
+
+            assert stream_id_A != stream_id_B
+
+            # Check that the 'saxpy' kernel ran on stream B.
+            cupti_activity_kind_kernel = report.table(name = 'CUPTI_ACTIVITY_KIND_KERNEL')
+            saxpy_kernel = self.single_row_to_dict(data = cupti_activity_kind_kernel)
+
+            assert saxpy_kernel['streamId'] == stream_id_B
+
+            # Check 'saxpy' kernel launch block/grid configuration.
+            assert saxpy_kernel['gridX'] == 8
+            assert saxpy_kernel['gridY'] == 1
+            assert saxpy_kernel['gridZ'] == 1
+
+            assert saxpy_kernel['blockX'] == 128
+            assert saxpy_kernel['blockY'] == 1
+            assert saxpy_kernel['blockZ'] == 1
+
+            # Check 'saxpy' kernel launch type.
+            ENUM_CUDA_KERNEL_LAUNCH_TYPE = report.table(name = 'ENUM_CUDA_KERNEL_LAUNCH_TYPE')
+
+            assert saxpy_kernel['launchType'] == self.single_row_to_dict(data = ENUM_CUDA_KERNEL_LAUNCH_TYPE[ENUM_CUDA_KERNEL_LAUNCH_TYPE['name'] == 'CUDA_KERNEL_LAUNCH_TYPE_REGULAR'])['id']
+
+            # Check 'saxpy' kernel mangled and demangled names.
+            stringids = report.table(name = 'StringIds')
+            assert self.single_row_to_dict(data = stringids[stringids['id'] == saxpy_kernel['mangledName'  ]])['value'] == '_Z12saxpy_kerneljfPKfPf'
+            assert self.single_row_to_dict(data = stringids[stringids['id'] == saxpy_kernel['demangledName']])['value'] == 'saxpy_kernel(unsigned int, float, const float *, float *)'

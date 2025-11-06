a54bbeeae3923b51088b3a841aeebb8f17a7500d
romintomasetti <romin.tomasetti@gmail.com>
2025-11-06 15:15:53 +0000

    tests(tools): cuobjdump many
    Ensure that if there are many functions in the dumped SASS, we get as many from our tool.
    
    Note that the __device__ functions are inlined.
    
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

---
+++ tests/python/tools/test_binaries/many.cu
@@ -0,0 +1,27 @@
+#include <cstdio>
+
+//! Atomically add 42.
+__device__ void atomic_add_42(float* __restrict__ const a, const unsigned int idx) {
+    atomicAdd(&a[idx], 42.f);
+}
+
+//! Add two @c float.
+__device__ float add(const float a, const float b) {
+    return a + b;
+}
+
+//! Basic parallel vector addition (with offset of 42) with bounds checking.
+__global__ void vector_atomic_add_42(const float* __restrict__ const a, const float* __restrict__ const b, float* __restrict__ const c, const unsigned int n)
+{
+    const unsigned int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (idx < n)
+    {
+        c[idx] = add(a[idx], b[idx]);
+        atomic_add_42(c, idx);
+    }
+}
+
+//! Say hi.
+__global__ void say_hi() {
+    printf("Hi from thread %d in block %d!\n", threadIdx.x, blockIdx.x);
+}

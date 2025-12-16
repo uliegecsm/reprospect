85f064b118052afbd4acb53a3402414f50e6afed
romintomasetti <romin.tomasetti@gmail.com>
2025-12-16 13:50:38 +0000

    test(sass): match a basic block with a given parent
    This strenghten even more the test.
    
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- reprospect/test/sass/controlflow/block.py
+++ reprospect/test/sass/controlflow/block.py
@@ -1,27 +1,82 @@
-import attrs
+import abc
+import sys
+import typing
 
-from reprospect.test.sass.composite_impl import InSequenceMatcher
-from reprospect.test.sass.instruction import InstructionMatch
+import mypy_extensions
+
+from reprospect.test.sass.composite_impl import InSequenceMatcher, SequenceMatcher
+from reprospect.test.sass.instruction import InstructionMatch, InstructionMatcher
 from reprospect.tools.sass.controlflow import BasicBlock, Graph
 
-@attrs.define(frozen=True, slots=True)
-class BlockMatcher:
-    """
-    Match the first block of a :py:class:`reprospect.tools.sass.controlflow.Graph` that
-    matches :py:attr:`matcher`.
-    """
-    matcher: InSequenceMatcher = attrs.field(converter=lambda x: x if isinstance(x, InSequenceMatcher) else InSequenceMatcher(x))
+if sys.version_info >= (3, 12):
+    from typing import override
+else:
+    from typing_extensions import override
 
+@mypy_extensions.mypyc_attr(allow_interpreted_subclasses=False)
+class BasicBlockMatcherBase(abc.ABC):
+    """
+    Base class for matchers that find blocks in a :py:class:`reprospect.tools.sass.controlflow.Graph`.
+    """
+    @abc.abstractmethod
+    def match(self, cfg: Graph) -> tuple[BasicBlock, list[InstructionMatch]] | None:
+        ...
+
+    def explain(self, cfg: Graph) -> str:
+        return f'{self!r} did not match any block of {cfg!r}.'
+
+    @typing.final
+    def assert_matches(self, cfg: Graph) -> tuple[BasicBlock, list[InstructionMatch]]:
+        if (matched := self.match(cfg=cfg)) is None:
+            raise RuntimeError(self.explain(cfg=cfg))
+        return matched
+
+class BasicBlockMatcher(BasicBlockMatcherBase):
+    """
+    Match the first block that matches :py:attr:`matcher`.
+
+    .. note::
+
+        It is not decorated with :py:func:`dataclasses.dataclass` because of https://github.com/mypyc/mypyc/issues/1061.
+    """
+    __slots__ = ('matcher',)
+
+    def __init__(self, matcher: InstructionMatcher|SequenceMatcher) -> None:
+        self.matcher: typing.Final[InSequenceMatcher] = matcher if isinstance(matcher, InSequenceMatcher) else InSequenceMatcher(matcher)
+
+    @override
     def match(self, cfg: Graph) -> tuple[BasicBlock, list[InstructionMatch]] | None:
         for block in cfg.blocks:
             if (matched := self.matcher.match(instructions=block.instructions)) is not None:
                 return block, matched
         return None
 
+    @override
     def explain(self, cfg: Graph) -> str:
-        return f'{self.matcher} did not match any block of {cfg}.'
+        return f'{self.matcher.matcher!r} did not match any block of {cfg!r}.'
 
-    def assert_matches(self, cfg: Graph) -> tuple[BasicBlock, list[InstructionMatch]]:
-        if (matched := self.match(cfg=cfg)) is None:
-            raise RuntimeError(self.explain(cfg=cfg))
-        return matched
+class BasicBlockWithParentMatcher(BasicBlockMatcherBase):
+    """
+    Match the first block of a :py:class:`reprospect.tools.sass.controlflow.Graph` that
+    matches :py:attr:`matcher` and has an incoming edge from :py:attr:`parent`.
+
+    .. note::
+
+        It is not decorated with :py:func:`dataclasses.dataclass` because of https://github.com/mypyc/mypyc/issues/1061.
+    """
+    __slots__ = ('matcher', 'parent')
+
+    def __init__(self, matcher: InstructionMatcher|SequenceMatcher, parent: BasicBlock) -> None:
+        self.matcher: typing.Final[InSequenceMatcher] = matcher if isinstance(matcher, InSequenceMatcher) else InSequenceMatcher(matcher)
+        self.parent: typing.Final[BasicBlock] = parent
+
+    @override
+    def match(self, cfg: Graph) -> tuple[BasicBlock, list[InstructionMatch]] | None:
+        for block in cfg.edges[self.parent]:
+            if (matched := self.matcher.match(instructions=block.instructions)) is not None:
+                return block, matched
+        return None
+
+    @override
+    def explain(self, cfg: Graph) -> str:
+        return f'{self.matcher.matcher!r} did not match any child of {self.parent!r}.'
--- tests/test/sass/controlflow/test_block.py
+++ tests/test/sass/controlflow/test_block.py
@@ -1,11 +1,12 @@
+import logging
 import pathlib
 import typing
 
 import pytest
 
-from reprospect.test.sass.controlflow.block import BlockMatcher
+from reprospect.test.sass.controlflow.block import BasicBlockMatcher, BasicBlockWithParentMatcher
 from reprospect.test.sass.composite import any_of, interleaved_instructions_are
-from reprospect.test.sass.instruction import LoadGlobalMatcher
+from reprospect.test.sass.instruction import OpcodeModsMatcher, LoadGlobalMatcher
 from reprospect.test.sass.instruction.half import Fp16MulMatcher, Fp16FusedMulAddMatcher
 from reprospect.tools.binaries import CuObjDump
 from reprospect.tools.sass.controlflow import ControlFlow
@@ -15,11 +16,7 @@ from reprospect.utils import cmake
 from tests.parameters import Parameters, PARAMETERS
 from tests.test.sass.test_instruction import get_compilation_output
 
-@pytest.mark.parametrize('parameters', PARAMETERS, ids=str, scope='class')
-class TestBlockMatcher:
-    """
-    Tests for :py:class:`reprospect.test.sass.controlflow.block.BlockMatcher`.
-    """
+class TestBasicBlock:
     FILE : typing.Final[pathlib.Path] = pathlib.Path(__file__).parent.parent.parent.parent / 'assets' / 'test_half.cu'
 
     SIGNATURE_INDIVIDUAL : typing.Final[str] = 'pow2_individual(__half *, const __half *, unsigned int)'
@@ -40,10 +37,15 @@ class TestBlockMatcher:
 
         return cuobjdump
 
+@pytest.mark.parametrize('parameters', PARAMETERS, ids=str, scope='class')
+class TestBasicBlockMatcher(TestBasicBlock):
+    """
+    Tests for :py:class:`reprospect.test.sass.controlflow.block.BasicBlockMatcher`.
+    """
     def test_find_individual_and_packed_blocks(self, parameters: Parameters, cuobjdump: CuObjDump) -> None:
         """
-        Find the "individual" block in :py:attr:`SIGNATURE_INDIVIDUAL`, and both "individual"
-        and "packed" blocks in :py:attr:`SIGNATURE_PACKED`.
+        Find the "individual" block in :py:attr:`TestBasicBlock.SIGNATURE_INDIVIDUAL`, and both "individual"
+        and "packed" blocks in :py:attr:`TestBasicBlock.SIGNATURE_PACKED`.
 
         Built on the observations from:
 
@@ -56,7 +58,7 @@ class TestBlockMatcher:
         assert len(cfg_individual.blocks) == 3
         assert len(cfg_packed.blocks) == 5
 
-        block_matcher_individual = BlockMatcher(matcher=interleaved_instructions_are(
+        block_matcher_individual = BasicBlockMatcher(matcher=interleaved_instructions_are(
             LoadGlobalMatcher(arch=parameters.arch, size=16, readonly=True, extend='U'),
             Fp16MulMatcher(packed=False),
         ))
@@ -64,7 +66,7 @@ class TestBlockMatcher:
         block_matcher_individual.assert_matches(cfg_individual)
         matched_individual_from_packed, _ = block_matcher_individual.assert_matches(cfg_packed)
 
-        block_matcher_packed = BlockMatcher(matcher=interleaved_instructions_are(
+        block_matcher_packed = BasicBlockMatcher(matcher=interleaved_instructions_are(
             LoadGlobalMatcher(arch=parameters.arch, size=32, readonly=True),
             any_of(Fp16MulMatcher(packed=True), Fp16FusedMulAddMatcher(packed=True)),
         ))
@@ -73,3 +75,30 @@ class TestBlockMatcher:
         matched_packed_from_packed, _ = block_matcher_packed.assert_matches(cfg_packed)
 
         assert matched_packed_from_packed != matched_individual_from_packed
+
+@pytest.mark.parametrize('parameters', PARAMETERS, ids=str, scope='class')
+class TestBasicBlockWithParentMatcher(TestBasicBlock):
+    """
+    Tests for :py:class:`reprospect.test.sass.controlflow.block.BasicBlockWithParentMatcher`.
+    """
+    def test_find_individual_block(self, parameters: Parameters, cuobjdump: CuObjDump) -> None:
+        """
+        Find the "individual" block in :py:attr:`TestBasicBlock.SIGNATURE_INDIVIDUAL`, that is the child of a block with an ``ISETP`` instruction.
+        """
+        cfg_individual = ControlFlow.analyze(instructions=Decoder(code=cuobjdump.functions[self.SIGNATURE_INDIVIDUAL].code).instructions)
+
+        assert len(cfg_individual.blocks) == 3
+
+        block_isetp, [matched_isetp] = BasicBlockMatcher(OpcodeModsMatcher(
+            opcode='ISETP', modifiers=('GE', 'U32', 'AND'), operands=True,
+        )).assert_matches(cfg=cfg_individual)
+        logging.info(matched_isetp)
+
+        block_individual, [matched_ldg, matched_mul] = BasicBlockWithParentMatcher(parent=block_isetp, matcher=interleaved_instructions_are(
+            LoadGlobalMatcher(arch=parameters.arch, size=16, readonly=True, extend='U'),
+            Fp16MulMatcher(packed=False),
+        )).assert_matches(cfg=cfg_individual)
+        logging.info(matched_ldg)
+        logging.info(matched_mul)
+
+        assert block_isetp != block_individual

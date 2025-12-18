077f8087999353de3ebcf544766bd9bea7ff1596
romintomasetti <romin.tomasetti@gmail.com>
2025-12-18 11:08:26 +0000

    test(sass): register matcher support for modifiers
    Signed-off-by: romintomasetti <romin.tomasetti@gmail.com>

--- tests/test/sass/instruction/test_register.py
+++ tests/test/sass/instruction/test_register.py
@@ -1,62 +1,81 @@
 import typing
 
-from reprospect.test.sass.instruction.register import RegisterMatch, RegisterType, RegisterMatcher
+import pytest
+
+from reprospect.test.sass.instruction.register import RegisterMatch, RegisterType, RegisterMatcher, RegisterModifier
 
 class TestRegisterMatcher:
     """
     Tests for :py:class:`reprospect.test.sass.instruction.register.RegisterMatcher`.
     """
+    REGISTERS: typing.Final[dict[str, RegisterMatch]] = {
+        'R42': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False),
+        '!R42': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False, modifier=RegisterModifier.NOT),
+        '~R42': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False, modifier=RegisterModifier.INV),
+        '-R42': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False, modifier=RegisterModifier.NEG),
+        '|R42|': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False, modifier=RegisterModifier.ABS),
+        'R42.reuse': RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = True),
+        'RZ': RegisterMatch(rtype = RegisterType.GPR, index = None, reuse = False),
+        'UR42': RegisterMatch(rtype = RegisterType.UGPR, index = 42, reuse = False),
+        'P3': RegisterMatch(rtype = RegisterType.PRED, index = 3, reuse = False),
+        'PT': RegisterMatch(rtype = RegisterType.PRED, index = None, reuse = False),
+        'UP3': RegisterMatch(rtype = RegisterType.UPRED, index = 3, reuse = False),
+        'UPT': RegisterMatch(rtype = RegisterType.UPRED, index = None, reuse = False),
+    }
+
+    MATCHER: typing.Final[RegisterMatcher] = RegisterMatcher()
+
     def test_reg(self) -> None:
         REG: typing.Final[str] = 'R42'
-        RES: typing.Final[RegisterMatch] = RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = False)
 
-        assert RegisterMatcher().match(REG) == RES
+        assert self.MATCHER.match(REG) == self.REGISTERS[REG]
 
         matcher = RegisterMatcher(rtype = RegisterType.GPR, special = False, reuse = False)
-        assert matcher.pattern.pattern == r'(?P<rtype>(R))(?P<index>(\d+))'
-        assert matcher.match(REG) == RES
+        assert matcher.pattern.pattern == r'(?:(?P<modifier>[\-!\|~]))?(?P<rtype>R)(?P<index>\d+)'
+        assert matcher.match(REG) == self.REGISTERS[REG]
 
     def test_regz(self) -> None:
         REG: typing.Final[str] = 'RZ'
-        RES: typing.Final[RegisterMatch] = RegisterMatch(rtype = RegisterType.GPR, index = None, reuse = False)
 
-        assert RegisterMatcher().match(REG) == RES
+        assert self.MATCHER.match(REG) == self.REGISTERS[REG]
 
         matcher = RegisterMatcher(rtype = RegisterType.GPR, special = True)
-        assert matcher.pattern.pattern == r'(?P<rtype>(R))(?P<special>(Z))'
-        assert matcher.match(REG) == RES
+        assert matcher.pattern.pattern == r'(?:(?P<modifier>[\-!\|~]))?(?P<rtype>R)(?P<special>Z)'
+        assert matcher.match(REG) == self.REGISTERS[REG]
 
     def test_ureg(self) -> None:
         REG: typing.Final[str] = 'UR42'
-        RES: typing.Final[RegisterMatch] = RegisterMatch(rtype = RegisterType.UGPR, index = 42, reuse = False)
 
-        assert RegisterMatcher().match(REG) == RES
+        assert self.MATCHER.match(REG) == self.REGISTERS[REG]
 
         matcher = RegisterMatcher(rtype = RegisterType.UGPR, special = False)
-        assert matcher.pattern.pattern == r'(?P<rtype>(UR))(?P<index>(\d+))(?P<reuse>\.reuse)?'
-        assert matcher.match(REG) == RES
+        assert matcher.pattern.pattern == r'(?:(?P<modifier>[\-!\|~]))?(?P<rtype>UR)(?P<index>\d+)(?P<reuse>\.reuse)?'
+        assert matcher.match(REG) == self.REGISTERS[REG]
 
     def test_reg_reuse(self) -> None:
-        assert RegisterMatcher().match('R42.reuse') == RegisterMatch(rtype = RegisterType.GPR, index = 42, reuse = True)
+        assert self.MATCHER.match('R42.reuse') == self.REGISTERS['R42.reuse']
 
     def test_pred(self) -> None:
-        assert RegisterMatcher().match('P3') == RegisterMatch(rtype = RegisterType.PRED, index = 3, reuse = False)
+        assert self.MATCHER.match('P3') == self.REGISTERS['P3']
 
     def test_predt(self) -> None:
         REG: typing.Final[str] = 'PT'
-        RES: typing.Final[RegisterMatch] = RegisterMatch(rtype = RegisterType.PRED, index = None, reuse = False)
 
-        assert RegisterMatcher().match(REG) == RES
+        assert self.MATCHER.match(REG) == self.REGISTERS[REG]
 
         matcher = RegisterMatcher(rtype = RegisterType.PRED)
-        assert matcher.pattern.pattern == r'(?P<rtype>(P))(?P<special>(T))?(?P<index>(\d+))?'
-        assert matcher.match(REG) == RES
+        assert matcher.pattern.pattern == r'(?:(?P<modifier>[\-!\|~]))?(?P<rtype>P)(?P<special>T)?(?P<index>\d+)?'
+        assert matcher.match(REG) == self.REGISTERS[REG]
 
     def test_upred(self) -> None:
-        assert RegisterMatcher().match('UP3') == RegisterMatch(rtype = RegisterType.UPRED, index = 3, reuse = False)
+        assert self.MATCHER.match('UP3') == self.REGISTERS['UP3']
 
     def test_upredt(self) -> None:
-        assert RegisterMatcher().match('UPT') == RegisterMatch(rtype = RegisterType.UPRED, index = None, reuse = False)
+        assert self.MATCHER.match('UPT') == self.REGISTERS['UPT']
 
     def test_not_a_reg(self) -> None:
-        assert RegisterMatcher().match('YOLO666') is None
+        assert self.MATCHER.match('YOLO666') is None
+
+    @pytest.mark.parametrize(('register', 'expected'), REGISTERS.items())
+    def test_any(self, register, expected) -> None:
+        assert self.MATCHER.match(reg=register) == expected

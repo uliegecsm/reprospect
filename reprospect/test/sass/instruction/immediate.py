cdf4400bf5b4904d6fbbe0fda132d7069620bbb5
Maarten Arnst <maarten.arnst@uliege.be>
2025-12-22 12:02:36 +0100

    Remove test immediate from compile output
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>

--- reprospect/test/sass/instruction/immediate.py
+++ reprospect/test/sass/instruction/immediate.py
@@ -10,7 +10,7 @@ class Immediate:
     INF: typing.Final[str] = r'[+-]?INF'
 
     QNAN: typing.Final[str] = PatternBuilder.any(r'[+-]?QNAN', '0x7fffffff')
-    """Quiet NaN. Note that in SASS, CUDART_NAN_F from math_constants.h is represented as 0x7fffffff."""
+    """Quiet NaN. Note that in SASS, :code:`CUDART_NAN_F` from ``math_constants.h`` is represented as ``0x7fffffff``."""
 
     FLOATING: typing.Final[str] = r'(?:-?\d+)(?:\.\d*)?(?:[eE][-+]?\d+)?'
     """
@@ -19,7 +19,7 @@ class Immediate:
     * https://github.com/cloudcores/CuAssembler/blob/96a9f72baf00f40b9b299653fcef8d3e2b4a3d49/CuAsm/CuInsParser.py#L34
     """
 
-    FLOATING_OR_LIMIT: typing.Final[str] = PatternBuilder.any(INF,QNAN,FLOATING)
+    FLOATING_OR_LIMIT: typing.Final[str] = PatternBuilder.any(INF, QNAN, FLOATING)
 
     @classmethod
     def floating(cls) -> str:
@@ -27,7 +27,7 @@ class Immediate:
         :py:attr:`FLOATING` with `operands` group.
         """
         return PatternBuilder.group(cls.FLOATING, group='operands')
-    
+
     @classmethod
     def floating_or_limit(cls) -> str:
         """
